473b26039dc7294c1deefb631f44904dad2a6fba
anti-machinee <81377071+anti-machinee@users.noreply.github.com>
2024-09-04 14:38:30 +0930

    dev

--- prac2/clockmmu.py
+++ prac2/clockmmu.py
@@ -1,112 +1,99 @@
 from mmu import MMU
 
 class ClockMMU(MMU):
-    def __init__(self, frames):
-        self.frames = frames
+    def __init__(self, num_frames):
+        self.num_frames = num_frames
         self.disk_reads = 0
         self.disk_writes = 0
         self.page_faults = 0
         self.debug_mode = False
 
-        # Memory to store pages (initially empty)
-        self.memory = []
+        # Dictionary to store the pages currently in memory
+        self.memory_map = {}
+        # Pointer for the clock hand in the algorithm
+        self.clock_pointer = 0
+        # Dictionary to keep track of reference bits
+        self.ref_bits = {}
+        # Dictionary to keep track of dirty bits (whether pages are modified)
+        self.dirty_bits = {}
+        # List to track the order of pages in memory
+        self.page_list = []
+        self.debug = False
 
-        # Page table to track pages in memory and their states (read/write)
-        self.page_table = {}
-
-        # Clock hand pointer to track the next page to be replaced
-        self.clock_hand = 0
-
-        # Use bits to track the second chance status of pages
-        self.use_bits = []
-
-    def set_debug(self):
+    def enable_debug(self):
         self.debug_mode = True
 
-    def reset_debug(self):
+    def disable_debug(self):
         self.debug_mode = False
 
-    def read_memory(self, page_number):
-        # Check if the page is already in memory
-        if page_number in self.page_table:
-            # Find the index of the page in memory and set its use bit to 1 (mark it as recently used)
-            frame_index = self.memory.index(page_number)
-            self.use_bits[frame_index] = 1
-            
+    def read_memory(self, page_id):
+        if page_id in self.memory_map:
+            # Page is present in memory
+            self.ref_bits[page_id] = 1
             if self.debug_mode:
-                print(f"Page {page_number} has been read from memory.")
-        
-        # If the page is not in memory, it's a page fault
+                print(f"Page {page_id} accessed in memory.")
         else:
+            # Page fault occurs
             self.page_faults += 1
-            self.handle_page_fault(page_number, mode='R')
-            if self.debug_mode:
-                print(f"Fail to read page {page_number} from memory.")
+            if len(self.memory_map) < self.num_frames:
+                # Space available in memory
+                self.memory_map[page_id] = len(self.memory_map)
+                self.page_list.append(page_id)
+                if self.debug_mode:
+                    print(f"Page {page_id} loaded into memory.")
+            else:
+                # Memory is full, replace a page
+                self.resolve_page_fault(page_id)
+            self.disk_reads += 1
+            self.ref_bits[page_id] = 1
+            self.dirty_bits[page_id] = 0
 
-    def write_memory(self, page_number):
-        # Check if the page is already in memory
-        if page_number in self.page_table:
-            # Find the index of the page in memory and set its use bit to 1 (mark it as recently used)
-            frame_index = self.memory.index(page_number)
-            self.use_bits[frame_index] = 1
-            
+    def write_memory(self, page_id):
+        if page_id in self.memory_map:
+            # Page is already in memory
+            self.ref_bits[page_id] = 1
+            self.dirty_bits[page_id] = 1
             if self.debug_mode:
-                print(f"Page {page_number} has been written to memory.")
-            
-            # Mark the page as 'W' (written)
-            self.page_table[page_number] = 'W'
-        
-        # If the page is not in memory, it's a page fault
+                print(f"Page {page_id} written to memory.")
         else:
-            if self.debug_mode:
-                print(f"Fail to write page {page_number} to memory.")
-            
+            # Page fault occurs
             self.page_faults += 1
-            self.handle_page_fault(page_number, mode='W')
+            if len(self.memory_map) < self.num_frames:
+                # Space available in memory
+                self.memory_map[page_id] = len(self.memory_map)
+                self.page_list.append(page_id)
+                if self.debug_mode:
+                    print(f"Page {page_id} loaded into memory.")
+            else:
+                # Memory is full, replace a page
+                self.resolve_page_fault(page_id)
+            self.disk_reads += 1
+            self.ref_bits[page_id] = 1
+            self.dirty_bits[page_id] = 1
 
-    def handle_page_fault(self, page_number, mode):
-        # If memory is full, use the clock algorithm to replace a page
-        if len(self.memory) == self.frames:
-            while True:
-                # Check if the current page pointed to by the clock hand has a use bit of 0
-                if self.use_bits[self.clock_hand] == 0:
-                    # If the use bit is 0, replace this page
-                    page_to_evict = self.memory[self.clock_hand]
-
-                    # If the evicted page was written to, increment disk writes
-                    if self.page_table[page_to_evict] == 'W':
-                        self.disk_writes += 1
-                    
-                    # Replace the evicted page with the new page
-                    self.memory[self.clock_hand] = page_number
-                    self.page_table[page_number] = mode
-                    del self.page_table[page_to_evict]
-                    self.use_bits[self.clock_hand] = 1
-
-                    if self.debug_mode:
-                        print(f"Page fault: page {page_number} has been loaded into frame {self.clock_hand}, replacing page {page_to_evict}.")
-                    
-                    # Exit the loop after handling the page fault
-                    break
-                
-                else:
-                    # If the use bit is 1, set it to 0 (give the page a second chance)
-                    self.use_bits[self.clock_hand] = 0
-
-                    # Move the clock hand to the next frame (circular movement)
-                    self.clock_hand = (self.clock_hand + 1) % self.frames
-        
-        else:
-            # If there is still space in memory, simply add the page
-            self.memory.append(page_number)
-            self.use_bits.append(1)
-            self.page_table[page_number] = mode
-
-            if self.debug_mode:
-                print(f"Page fault: page {page_number} has been loaded into memory.")
-
-        # Increment disk reads when a new page is loaded into memory
-        self.disk_reads += 1
+    def resolve_page_fault(self, new_page):
+        while True:
+            # Select the page to be replaced based on the clock hand
+            candidate_page = self.page_list[self.clock_pointer]
+            if self.ref_bits[candidate_page] == 0:
+                # The page is not recently used
+                if self.dirty_bits[candidate_page] == 1:
+                    # Increment disk writes if the page was modified
+                    self.disk_writes += 1
+                if self.debug_mode:
+                    print(f"Replacing page {candidate_page} with new page {new_page}.")
+                # Replace the candidate page with the new page
+                del self.memory_map[candidate_page]
+                self.memory_map[new_page] = self.clock_pointer
+                self.page_list[self.clock_pointer] = new_page
+                self.ref_bits[new_page] = 1
+                self.dirty_bits[new_page] = 0
+                self.clock_pointer = (self.clock_pointer + 1) % self.num_frames
+                break
+            else:
+                # Provide a second chance to the page
+                self.ref_bits[candidate_page] = 0
+                self.clock_pointer = (self.clock_pointer + 1) % self.num_frames
 
     def get_total_disk_reads(self):
         return self.disk_reads
